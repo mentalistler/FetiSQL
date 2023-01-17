b463c8a2c3b5dbbb7cd976f04a1ba458e556ac51
mentalistler <121824502+mentalistler@users.noreply.github.com>
2023-01-18 01:21:28 +0300

    Update DorkScanner.py

--- tools/DorkScanner.py
+++ tools/DorkScanner.py
@@ -5,24 +5,9 @@ import re
 import threading
 from tqdm import tqdm
 results = []
-def search_duckduckgo(query,visual):
-    try:
-        global results
-        user_agent = {'User-agent': useragent.get_useragent()}
-        response = requests.get(f"https://html.duckduckgo.com/html/?q={query}",headers=user_agent,timeout=10)
-        urls = re.findall(r'<a class="result__url" href="(.+?)">', response.text)
-        for i in range(len(urls)):
-            if urls[i].startswith('//duckduckgo.com/l/?uddg='):
-                real_url = re.search(r'uddg=(.+?)&', urls[i]).group(1)
-                urls[i] = real_url
-        results += urls
-    except:
-        pass
-    visual.update(1)
 
 def search_ask(query,visual):
     global results
-    search_duckduckgo(query,visual)
     for i in range(1,20):
         try:
             user_agent = {'User-agent': useragent.get_useragent()}
@@ -32,17 +17,48 @@ def search_ask(query,visual):
         except:
             pass
         visual.update(1)
-
-def SearchMain(dork,thread):
+        
+def search_searchencrypt(query,visual):
+    global results
+    try:
+        for j in range(1,20):
+            user_agent = {'User-agent': useragent.get_useragent()}
+            response = requests.get(f"https://spapi.searchencrypt.com/api/search?q={query}&types=web&page={j}&limit=20",headers=user_agent)
+            res = response.json()["Results"]
+            for i in range(len(res)):
+                results.append(res[i]["ClickUrl"])
+    except:
+        pass
+    visual.update(1)
+def search_neeva(query,visual):
+    global results
+    for i in range(1,20):
+        try:
+            user_agent = {'User-agent': useragent.get_useragent()}
+            response = requests.get(f"https://neeva.com/search?q={query}&c=All&src=Pagination&page={i}",headers=user_agent)
+            urls = re.findall(r'<a[^>]+href="(https?:\/\/[^"]+)"', response.text)
+            results += urls
+        except:
+            pass
+        visual.update(1)
+def SearchMain(dork,thread,engine):
     threads = []
     ttl = len(dork)
-    visual = tqdm(total=ttl*20,desc=f' Scanning Dorks')
+    visual = tqdm(total=ttl*19,desc=f' Scanning Dorks')
     lines = open(dork, "r").read().splitlines()
     i = 0
     while(i < len(lines)):
         for j in range(i,min(i+thread,len(lines))):
             try:
-                t = threading.Thread(target=search_ask,args=(lines[j],visual))
+                if(engine=="neeva"):
+                    t = threading.Thread(target=search_neeva,args=(lines[j],visual))
+                elif(engine=="ask"):
+                    t = threading.Thread(target=search_ask,args=(lines[j],visual))
+                elif(engine=="searchencrypt"):
+                    t = threading.Thread(target=search_searchencrypt,args=(lines[j],visual))
+                else:
+                    colorprint.colorprint("Engine Error","v")
+                    exit()
                 threads.append(t)
                 t.start()
             except:
